f0be9526203ccd32f4b210a8fd88513b889ec1ef
Rohan <guptarohan811@gmail.com>
2021-03-08 13:59:14 +0530

    Updation

--- app/src/main/java/com/example/unidayz/DashboardActivity.kt
+++ app/src/main/java/com/example/unidayz/DashboardActivity.kt
@@ -1,12 +1,33 @@
 package com.example.unidayz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val profile : BottomNavigationItemView = findViewById(R.id.nav_profile)
+        val msg : BottomNavigationItemView = findViewById(R.id.nav_message)
+        val not : BottomNavigationItemView = findViewById(R.id.nav_social)
+
+        profile.setOnClickListener{
+            startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }
+        msg.setOnClickListener{
+            startActivity(Intent(this, MessageActivity::class.java))
+            finish()
+        }
+        not.setOnClickListener{
+            startActivity(Intent(this, NotifActivity::class.java))
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/unidayz/ProfileActivity.kt
+++ app/src/main/java/com/example/unidayz/ProfileActivity.kt
@@ -1,11 +1,31 @@
 package com.example.unidayz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val dashBoard : BottomNavigationItemView = findViewById(R.id.nav_sch)
+        val msg : BottomNavigationItemView = findViewById(R.id.nav_message)
+        val not : BottomNavigationItemView = findViewById(R.id.nav_social)
+
+        dashBoard.setOnClickListener{
+            startActivity(Intent(this, DashboardActivity::class.java))
+            finish()
+        }
+        msg.setOnClickListener{
+            startActivity(Intent(this, MessageActivity::class.java))
+            finish()
+        }
+        not.setOnClickListener{
+            startActivity(Intent(this, NotifActivity::class.java))
+            finish()
+        }
+
     }
 }
