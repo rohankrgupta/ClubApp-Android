fc185b5e8022215849edcac8731eef9b3a29f503
Rohan <guptarohan811@gmail.com>
2021-03-08 17:17:01 +0530

    Updation

--- app/src/main/java/com/example/unidayz/DashboardActivity.kt
+++ app/src/main/java/com/example/unidayz/DashboardActivity.kt
@@ -4,14 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        val recycler_view : RecyclerView = findViewById(R.id.dashrecycler)
         val profile : BottomNavigationItemView = findViewById(R.id.nav_profile)
         val msg : BottomNavigationItemView = findViewById(R.id.nav_message)
         val not : BottomNavigationItemView = findViewById(R.id.nav_social)
@@ -28,6 +35,14 @@ class DashboardActivity : AppCompatActivity() {
             startActivity(Intent(this, NotifActivity::class.java))
             finish()
         }
+
+        setSupportActionBar(toolbar)
+        layoutManager = LinearLayoutManager(this)
+        recycler_view.layoutManager = layoutManager
+
+        adapter = RecyclerAdapter()
+        recycler_view.adapter = adapter
+
 
     }
 }
--- app/src/main/java/com/example/unidayz/profileRecyclerAdapter.kt
+++ app/src/main/java/com/example/unidayz/profileRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.unidayz
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -7,40 +8,42 @@ import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.profile_list_view.view.*
-/*
-class profileRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
+    private val posts = arrayOf("Robocon, Nit Durgapur", "Generation Google Scholarship", "Training and Placement Representative election"
+    , "AtCoder Beginner Contest 194 ")
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    private val details = arrayOf(
+        "Well, here we, Robocon, Nit Durgapur are giving you a small insite on what's Robocon is and what all we actually do, here's a video for you all people to light a fire in ur hearts.\nDo come in auditions of Team Robocon",
+        "We are excited to share that the applications for Generation Google Scholarship: for women in computer science (formerly known as Women Techmakers Scholarship) are now open! ",
+        "With all the due efforts and arrangements of the TPRs and the office of the Training and Placement, the 2nd year Training and Placement Representative election is now ready to happen.",
+        "AtCoder Beginner Contest 194 will start on 6th March 2021 at 5:30 PM IST.\nThe contest duration is 100 minutes.\n \n Contest Link : https://atcoder.jp/contests/abc194\n \n Happy Coding"
+    )
 
+    inner class  ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemTitle : TextView
+        var itemDetail : TextView
+
+        init {
+            itemTitle = itemView.findViewById(R.id.pr_title)
+            itemDetail = itemView.findViewById(R.id.pr_des)
+        }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder  {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.profile_list_view, viewGroup, false)
+        return ViewHolder(v)
+    }
 
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = posts[i]
+        viewHolder.itemDetail.text = details[i]
     }
 
     override fun getItemCount(): Int {
-
-    }
-
-    class profileViewHolder constructor(
-        itemView: View
-    ): RecyclerView.ViewHolder(itemView){
-        val backImage: ImageView = itemView.back_image
-        val profImage: ImageView = itemView.profile_pic
-        val profName: TextView = itemView.pr_name
-        val profDegree: TextView = itemView.pr_degree
-        val profUniversity:TextView = itemView.pr_uni
-        val profUniAddress: TextView = itemView.pr_add
-        val profFriendsCnt: TextView = itemView.friends_list
-        val profDescription: TextView = itemView.pr_des
-
-
-
-
-
+        return posts.size
     }
 
 
-}*/
+}
